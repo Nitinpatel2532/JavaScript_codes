96d3edebad4b1f98c67a8936d9f70b39a5181aad
Nitinpatel2532 <nitinpatel74084@gmail.com>
2025-02-02 19:43:53 +0530

    success

---
+++ 34_fetchdata_printinttable.js
@@ -0,0 +1,24 @@
+
+async function fetch_data() {
+    let fetching = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let final = await fetching.json();
+
+    // console.log(final)
+
+    let final_data = final.map((data) => `
+    <tr>
+    <td> ${data.userId} </td>
+       <td>${data.id}  </td>
+          <td>${data.title}</td>
+
+
+    </tr>
+    
+    `).join("")
+
+
+    document.querySelector("#Displaydata").innerHTML=final_data
+
+    
+}
+fetch_data()
